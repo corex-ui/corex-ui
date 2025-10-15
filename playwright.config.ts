816b98491cb341b739393267e50e2ce3b0929927
karimsemmoud <karim.semmoud@netoum.com>
2025-10-15 11:49:06 +0200

    dev(test):adding testing

---
+++ playwright.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./e2e",
+  outputDir: "./e2e/_test-results",
+  projects: [
+    {
+      name: "vite-chromium-dist",
+      use: {
+        ...devices["Desktop Chrome"],
+        baseURL: "http://localhost:4173",
+      },
+    },
+  ],
+  reporter: [["html", { outputFolder: "e2e/report", open: "never" }]],
+  webServer: {
+    command:
+      "pnpm --prefix examples/static-vite build && pnpm --prefix examples/static-vite preview --port 4173 --host",
+    port: 4173,
+    reuseExistingServer: !process.env.CI,
+  },
+});
