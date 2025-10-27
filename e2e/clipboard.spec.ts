7b0963d7fe7c591eccd6cdc264222267315898ee
karimsemmoud <karim.semmoud@netoum.com>
2025-10-27 14:52:54 +0100

    add(tests): add new tests

--- e2e/clipboard.spec.ts
+++ e2e/clipboard.spec.ts
@@ -9,4 +9,21 @@ test.describe("Clipboard", () => {
   test("should have no accessibility violations", async ({ page }) => {
     await a11y(page);
   });
+
+  test("should copy value to clipboard on trigger click", async ({ page }) => {
+    const root = page
+      .locator('[data-test="basic-clipboard"] [data-part="root"]')
+      .first();
+    const input = root.locator('[data-part="input"]');
+    const trigger = root.locator('[data-part="trigger"]');
+
+    await trigger.click();
+
+    await expect(root).toHaveAttribute("data-copied", "");
+
+    expect(await input.inputValue()).toBe("info@netoum.com");
+
+    await page.waitForTimeout(5000); // matches data-timeout
+    expect(await root.getAttribute("data-copied")).toBeNull();
+  });
 });
