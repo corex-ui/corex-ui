e826d5fc1f60bb2934b266b5e9bdbf9c997d60bd
karimsemmoud <karim.semmoud@netoum.com>
2025-10-29 10:12:55 +0100

    test(clipboard): add behaviour test

--- e2e/clipboard.spec.ts
+++ e2e/clipboard.spec.ts
@@ -9,4 +9,21 @@ test.describe("Clipboard", () => {
   test("should have no accessibility violations", async ({ page }) => {
     await a11y(page);
   });
+
+  test("should copy value to clipboard on trigger click", async ({ page }) => {
+    const { parts } = getComponent(page, "basic-clipboard", [
+      "root",
+      "trigger",
+      "input",
+    ]);
+
+    await parts.trigger.click();
+
+    await expect(parts.root).toHaveAttribute("data-copied", "");
+
+    expect(await parts.input.inputValue()).toBe("info@netoum.com");
+
+    await page.waitForTimeout(5000);
+    expect(await parts.root.getAttribute("data-copied")).toBeNull();
+  });
 });
