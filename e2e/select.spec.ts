c67706b7b278a7cb8baaf40ca7aa28dddd7f451b
karimsemmoud <karim.semmoud@netoum.com>
2025-10-31 06:44:29 +0100

    test(select): add delay to keyboard nav

--- e2e/select.spec.ts
+++ e2e/select.spec.ts
@@ -112,10 +112,10 @@ test.describe("Select", () => {
       const secondItem = items.nth(1);
 
       await page.keyboard.press("ArrowDown");
-      await expect(firstItem).toHaveAttribute("data-highlighted", "");
+      await expect(firstItem).toHaveAttribute("data-highlighted", "", { timeout: 2000 });
 
       await page.keyboard.press("ArrowDown");
-      await expect(secondItem).toHaveAttribute("data-highlighted", "");
+      await expect(secondItem).toHaveAttribute("data-highlighted", "", { timeout: 2000 });
 
       await page.keyboard.press("Enter");
       await expect(trigger).toHaveText(/Euro/);
