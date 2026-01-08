ce861c9451e6c60b16a5b9d70088ae172cdc2bcd
karimsemmoud <karim.semmoud@netoum.com>
2026-01-08 10:00:50 +0700

    release(QrCode): Ad events, fix layout and doc

--- doc/src/pages/qr-code.ts
+++ doc/src/pages/qr-code.ts
@@ -3,3 +3,9 @@ import { initQrCode } from "@corex-ui/static";
 import "./qr-code.css";
 
 initQrCode();
+
+document
+  .getElementById("my-qr-code")
+  ?.addEventListener("my-qr-code-event", (event) => {
+    console.log("Received event:", (event as CustomEvent).detail);
+  });
